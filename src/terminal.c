9c340d044c7054e72fa1db3779cd1cedf36d3026
ruter <arturkummer08@gmail.com>
2025-07-03 11:41:49 +0200

    bugfix

--- src/terminal.c
+++ src/terminal.c
@@ -70,6 +70,13 @@ void terminal_putchar(char c) {
             terminal_scroll();
             terminal_row = VGA_HEIGHT - 1;
         }
+    } else if (c == '\b') {
+        // Handle backspace
+        if (terminal_column > 0) {
+            terminal_column--;
+            // Clear the character at the current position
+            terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+        }
     } else {
         terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
         if (++terminal_column == VGA_WIDTH) {
