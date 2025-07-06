b39c809bb85a18ccaea88f30290f6626c88c5e4d
ruter <arturkummer08@gmail.com>
2025-07-06 14:40:57 +0200

    added SHIFT

--- src/drivers/keyboard.c
+++ src/drivers/keyboard.c
@@ -4,8 +4,15 @@
 // A very simple keyboard driver
 
 #define KBD_DATA_PORT 0x60
+#define LEFT_SHIFT_SCANCODE 0x2A
+#define RIGHT_SHIFT_SCANCODE 0x36
+#define LEFT_SHIFT_RELEASE 0xAA
+#define RIGHT_SHIFT_RELEASE 0xB6
 
-// US keyboard layout
+// Track shift key state
+static int shift_pressed = 0;
+
+// US keyboard layout (lowercase)
 static const char kbd_us[128] = {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',   /* Backspace */
     '\t',         /* Tab */
@@ -13,6 +20,42 @@ static const char kbd_us[128] = {
     0,          /* 29   - Control */
     'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',  0,    /* Left shift */
     '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',   0,      /* Right shift */
+    '*',
+    0,  /* Alt */
+    ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+    '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+    '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
+
+// US keyboard layout (uppercase/shifted)
+static const char kbd_us_shift[128] = {
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',   /* Backspace */
+    '\t',         /* Tab */
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', /* Enter key */
+    0,          /* 29   - Control */
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',  0,    /* Left shift */
+    '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?',   0,      /* Right shift */
     '*',
     0,  /* Alt */
     ' ',  /* Space bar */
@@ -51,9 +94,22 @@ char keyboard_getchar() {
         // Wait for a key press
         if (inb(0x64) & 1) {
             scancode = inb(KBD_DATA_PORT);
+            
+            // Handle shift key presses and releases
+            if (scancode == LEFT_SHIFT_SCANCODE || scancode == RIGHT_SHIFT_SCANCODE) {
+                shift_pressed = 1;
+                continue;
+            } else if (scancode == LEFT_SHIFT_RELEASE || scancode == RIGHT_SHIFT_RELEASE) {
+                shift_pressed = 0;
+                continue;
+            }
+            
             // Return the character if it's a valid key press
-            if (scancode < 128 && kbd_us[scancode]) {
-                return kbd_us[scancode];
+            if (scancode < 128) {
+                char c = shift_pressed ? kbd_us_shift[scancode] : kbd_us[scancode];
+                if (c) {
+                    return c;
+                }
             }
         }
     }
