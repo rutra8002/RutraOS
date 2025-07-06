28974f4ab4a465f2e7e740e4eae443a11b091c92
ruter <arturkummer08@gmail.com>
2025-07-06 12:21:23 +0200

    the nuclear option that works everywhere

--- src/shell.c
+++ src/shell.c
@@ -4,6 +4,7 @@
 #include "memory.h"
 #include "ramdisk.h"
 #include "fat12.h"
+#include "io.h"
 
 typedef unsigned long size_t;
 
@@ -59,11 +60,21 @@ void shell_init() {
 void shell_shutdown() {
     terminal_writestring("Shutting down system...\n");
     
-    // ACPI shutdown for most systems
+    // Try ACPI shutdown first (works on most modern systems)
     __asm__ volatile("outw %0, %1" : : "a"((unsigned short)0x2000), "d"((unsigned short)0x604));
     
-    // If we reach here, shutdown failed
-    terminal_writestring("Shutdown failed.\n");
+    // If ACPI fails, try QEMU/Bochs shutdown
+    __asm__ volatile("outw %0, %1" : : "a"((unsigned short)0x2000), "d"((unsigned short)0xB004));
+    
+    // If that fails too, force a triple fault 
+    // As copilot said - the nuclear option that works everywhere
+    terminal_writestring("Forcing system reset...\n");
+    __asm__ volatile("cli");           // Disable interrupts
+    __asm__ volatile("lidt %0" : : "m"(*(short*)0)); // Load invalid IDT
+    __asm__ volatile("int $0x03");     // Trigger interrupt with invalid IDT
+    
+    // This should never be reached
+    __asm__ volatile("hlt");
 }
 
 void shell_handle_input(char c) {
